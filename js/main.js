71b565a2ac05b12f28975b4cb40e2f420e26752a
staro <staro.oleg@gmail.com>
2023-05-25 17:36:28 +0300

    add section home

--- js/main.js
+++ js/main.js
@@ -1,41 +1,43 @@
 /*=============== SHOW MENU ===============*/
-const navMenu =document.getElementById('nav-menu'),
-	navToggle =document.getElementById('nav-toggle'),
-	navClose =document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 if (navToggle) {
-	navToggle.addEventListener('click', () => {
-		navMenu.classList.add('show-menu')
-	})
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
 }
 
 if (navClose) {
-	navClose.addEventListener('click', () => {
-		navMenu.classList.remove('show-menu')
-	})
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-	const navMenu =document.getElementById('nav-menu')
+    const navMenu = document.getElementById('nav-menu')
 
-	navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu')
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
 
+    this.scrollY >= 50 ? nav.classList.add('bg-header') : nav.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
